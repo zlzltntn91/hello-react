6fbb33a3960708428c8b21a03518f4317b8eaa52
zlzltntn91 <zlamstn91@gmail.com>
2021-03-20 09:33:45 +0900

    chapter 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 import UsePropsAndChildrenFunction from "./mysrc/UsePropsAndChildrenFunction";
 import PropsAndChildren from "./mysrc/UsePropsAndChildrenClass";
 import StateFunction from "./mysrc/StateFunction";
 import StateClass from "./mysrc/StateClass";
 import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
 
 const App = () => {
-    return (
-        <EventPractice/>
-    )
-}
+  return <ValidationSample></ValidationSample>;
+};
 
 export default App;
---
+++ src/ValidationSample.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./ValidationSample.css";
+class ValidationSample extends Component {
+  state = {
+    password: "",
+    clicked: false,
+    validated: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.clicked,
+      validated: this.password === "1219",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={
+            this.clicked === true
+              ? this.validated === true
+                ? "success"
+                : "failure"
+              : ""
+          }
+        />
+        <button type="button" onClick={this.handleClick}>
+          검증하기
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ValidationSample;
