427e1ba6e4e9b9059490b60cc1299cb4451df49e
pamat <zlamstn91@gmail.com>
2025-07-15 05:08:43 +0900

    test

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import React, { useState } from "react";
 import LifeCycleSample from "./7LifeCycle/LifeCycleSample";
 
 const App = () => {
-  const [state, setState] = useState({
-    color: "#000000",
-  });
+  const [state, setState] = useState("#000000");
 
   const getRandomColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
   };
 
   const handeClick = () => {
-    setState({
-      color: getRandomColor(),
-    });
+    setState(getRandomColor());
   };
 
   return (
     <div>
       <button onClick={handeClick}>랜덤색상</button>
-      <LifeCycleSample color={state.color} />
+      <LifeCycleSample color={state} />
     </div>
   );
 };
